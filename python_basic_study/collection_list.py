84c47b9d2f7e9f5eea726e8ff1e787daac36268a
SeungHyeonTak-sub <conficker77@gmail.com>
2022-01-08 18:26:50 +0900

    array commit

---
+++ python_basic_study/collection_list.py
@@ -0,0 +1,14 @@
+# 리스트가 없다면?
+person1_name = '홍길동'
+person1_age = '33'
+person1_address = 'asdasdasd'
+person1_tel = '010-1111-1111'
+
+print(person1_name)
+print(person1_age)
+print(person1_address)
+print(person1_tel)
+
+# 리스트가 있다면
+person1 = ['홍길동', 33, 'asdlajsdla', '010-2222-2222']
+print(person1)
